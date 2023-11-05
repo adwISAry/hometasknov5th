50728747a183805c28e15316525ece03e4b27603
adwISAry <mi75axs6d@code.edu.az>
2023-11-05 23:52:45 +0400

    Add project files.

---
+++ hometasknov5th/MyList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class MyList<T>
+{
+    private T[] items;  
+    private int count;  
+
+    public int Capacity { get; private set; }  
+
+    public int Count { get { return count; } }  
+
+   
+    public MyList()
+    {
+        Capacity = 4;
+        items = new T[Capacity]; 
+        count = 0;  
+    }
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException(); 
+            }
+            return items[index];  
+        }
+        set
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException();  
+            }
+            items[index] = value;  
+        }
+    }
+    public void Add(T item)
+    {
+        if (count == Capacity)
+        {
+            Capacity *= 2;  
+            Array.Resize(ref items, Capacity);  
+        }
+        items[count] = item;  
+        count++;  
+    }
+    public void Clear()
+    {
+        count = 0;  
+        Array.Clear(items, 0, Capacity);  
+    }
+    //public bool Exist(T item)
+    //{
+    //   
+    //}
+    //public bool Remove(T item)
+    //{
+      
+    //}
+
+    public void Reverse()
+    {
+        Array.Reverse(items, 0, count); 
+    }
+    public int IndexOf(T item)
+    {
+        return Array.IndexOf(items, item, 0, count);  
+    }
+    public int LastIndexOf(T item)
+    {
+        return Array.LastIndexOf(items, item, count - 1, count);  
+    }
+}
---
+++ hometasknov5th/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        MyList<int> myList = new MyList<int>(); 
+
+        while (true)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Add an integer");
+            Console.WriteLine("2. Get value at index");
+            Console.WriteLine("3. Set value at index");
+            Console.WriteLine("4. Remove a value"); //islemir yaza bilmedim
+            Console.WriteLine("5. Clear the list");
+            Console.WriteLine("6. Get the count of items");
+            Console.WriteLine("7. Exit");
+
+            Console.Write("Enter your choice (1-7): ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Enter an integer to add:");
+                    int addValue = Convert.ToInt32(Console.ReadLine());
+                    myList.Add(addValue);
+                    break;
+
+                case "2":
+                    Console.WriteLine("Enter an index to get:");
+                    int getIndex = Convert.ToInt32(Console.ReadLine());
+                    try
+                    {
+                        int value = myList[getIndex];
+                        Console.WriteLine($"Value at index {value}");
+
+
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("Invalid index. Try again.");
+                    }
+                    break;
+
+                case "3":
+                    Console.WriteLine("Enter an index to set:");
+                    int setIndex = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter a value to set:");
+                    int setValue = Convert.ToInt32(Console.ReadLine());
+                    try
+                    {
+                        myList[setIndex] = setValue;
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("Invalid index. Try again.");
+                    }
+                    break;
+
+                //case "4":
+                   
+                //    break;
+
+                case "5":
+                    myList.Clear();
+                    Console.WriteLine("List cleared.");
+                    break;
+
+                case "6":
+                    Console.WriteLine($"Count: {myList.Count}");
+                    break;
+
+                case "7":
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+}
